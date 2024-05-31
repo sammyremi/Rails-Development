39c245f05d0fa93c873771eade5708aca754389e
sammyremi <adebayorsamuelas@gmail.com>
2024-05-31 13:04:52 +0100

    using variable in ruby

---
+++ using_variable.rb
@@ -0,0 +1,18 @@
+a = 10
+
+b = 20
+
+puts a.object_id
+
+puts b.object_id
+
+puts a.class
+#object ID is used to get the variable memory address
+#
+#QUESTIONS
+#
+#1. What do you mean by variable: A variable is a place holder for values which are stored in the computer memory
+#2. what are variables in Ruby?:variables are object of classes. 
+#3. What are constant in Ruby?: constant are variables when declared the value cannot be changed
+#4. How to define variables in Ruby?: a = 10, b = 20
+#5. How to get memory address of a variable in ruby?: a = 10, a.object_id
