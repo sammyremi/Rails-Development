5b62f3325267bdc99b53df0d2ea2d2dd2ae8257c
sammyremi <adebayorsamuelas@gmail.com>
2024-05-31 20:43:49 +0100

    ruby classes

--- student_class.rb
+++ student_class.rb
@@ -6,7 +6,22 @@ class Student
         @contact = contact
     end
 
+    def print_name
+        puts @name
+    end
 
+    def print_email
+        puts @email
+    end
 
+    def print_contact
+        puts @contact
+    end
 
 end
+
+student1 = Student.new("sammyremi", "admin@admin.com", "08147468979")
+
+student1.print_name
+student1.print_contact
+student1.print_email
